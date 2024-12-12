244ddbec29fcdc4f59fe5fa5488e580702c61e1a
lechatn <noe.lechat@ynov.com>
2024-12-12 14:49:49 +0100

    add car file

--- Car.cs
+++ Car.cs
@@ -3,6 +3,25 @@ using System;
 namespace CsharpFinalProject {
     public class Car
     {
-        
+        public string Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public Car(string Brand, string Model, int Year)
+        {
+            Guid myUUId = Guid.NewGuid();
+            string convertedUUID = myUUId.ToString();
+            this.Id = convertedUUID;
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Year = Year;
+            this.IsAvailable = true;
+        }
+
+        public override string ToString(){
+            return $"{Brand} {Model} {Year} {IsAvailable}";
+        }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CsharpFinalProject;
+using System;
+
+namespace CsharpFinalProject
+{
+    class Program
+    {
+        static void Main(string[] str)
+        {
+            Car test = new Car("peugeot","206",2014);
+            Console.WriteLine(test);
+        }
+    }
+}
